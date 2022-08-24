da8ede3ad8eea3bca5512fb380bd8d22b6151e8b
rkdmf7220 <rkdmf7220@gmail.com>
2022-08-24 17:05:30 +0900

    1. 컴포넌트 구조 변경 2. 궁 온 시간 추가 3. 스타일링

--- src/components/AbilityHasteCalculator.vue
+++ src/components/AbilityHasteCalculator.vue
@@ -1,49 +1,67 @@
 <template>
   <div class="wrap">
-    <div class="input-area-container border-box">
-      <div class="input-item-container">
-        <label for="champion-names" @input="onSelectChampion">챔피언 : </label>
-        <input type="text" list="champion-names" v-model="inputChampionName" class="champion-names">
-        <datalist id="champion-names">
-          <option v-for="item in championNames" :key="item" :value="item"></option>
-        </datalist>
+    <div class="border-box">
+      <div class="input-area-container item-container">
+        <div class="input-item-container">
+          <label for="champion-names" @input="onSelectChampion">챔피언 : </label>
+          <input type="text" list="champion-names" v-model="inputChampionName" class="champion-names">
+          <datalist id="champion-names">
+            <option v-for="item in championNames" :key="item" :value="item"></option>
+          </datalist>
+        </div>
+
+        <div class="input-item-container">
+          <input @change="useUltimateHunter = !useUltimateHunter" type="checkbox" name="" id="check-ultimate-hunter">
+          <label for="check-ultimate-hunter">궁극의 사냥꾼</label>
+          <input :disabled="!useUltimateHunter" v-model="inputUltimateHunter" type="number" id="ultimate-hunter" min="0"
+                 max="5">
+        </div>
+
+        <div class="input-item-container">
+          <label for="ability-haste">스킬 가속 : </label>
+          <input v-model="inputHasteText" type="number" id="ability-haste" minlength="1" maxlength="3" min="0">
+        </div>
+
+        <div class="input-item-container">
+          <label for="current-time">게임 시간 : </label>
+          <input v-model="inputCurrentTime" type="text" id="current-time" minlength="4" maxlength="4">
+        </div>
       </div>
 
-      <div class="input-item-container">
-        <input @change="useUltimateHunter = !useUltimateHunter" type="checkbox" name="" id="check-ultimate-hunter">
-        <label for="check-ultimate-hunter">궁극의 사냥꾼</label>
-        <input :disabled="!useUltimateHunter" v-model="inputUltimateHunter" type="number" id="ultimate-hunter" min="0"
-               max="5">
+      <div class="calculator item-container">
+        <div class="calc-item small-text">
+          <span>스킬</span>
+          <span>쿨타임</span>
+          <span>궁 온</span>
+        </div>
+        <div class="calc-item">
+          <span class="calc-name"> 궁 1렙 : </span>
+          <span class="calc-value">{{ computeUltimateLevel1 }}</span>
+          <span class="calc-time">{{ moment(inputCurrentTime, 'mm:ss').add(computeUltimateLevel1, 'seconds').format('mm:ss') }}</span>
+        </div>
+        <div class="calc-item">
+          <span class="calc-name"> 궁 2렙 : </span>
+          <span class="calc-value">{{ computeUltimateLevel2 }}</span>
+          <span class="calc-time">{{ moment(inputCurrentTime, 'mm:ss').add(computeUltimateLevel2, 'seconds').format('mm:ss') }}</span>
+        </div>
+        <div class="calc-item">
+          <span class="calc-name"> 궁 3렙 : </span>
+          <span class="calc-value">{{ computeUltimateLevel3 }}</span>
+          <span class="calc-time">{{ moment(inputCurrentTime, 'mm:ss').add(computeUltimateLevel3, 'seconds').format('mm:ss') }}</span>
+        </div>
       </div>
-
-      <div class="input-item-container">
-        <label for="ability-haste">스킬 가속 : </label>
-        <input v-model="inputHasteText" type="number" id="ability-haste" minlength="1" maxlength="3" min="0">
-      </div>
-    </div>
-    <div class="calculator border-box">
-      <div class="calc-item">
-        <span class="calc-name"> 궁 1렙 : </span><span class="calc=value">{{ computeUltimateLevel1 }}</span>
-      </div>
-      <div class="calc-item">
-        <span class="calc-name"> 궁 2렙 : </span><span class="calc=value">{{ computeUltimateLevel2 }}</span>
-      </div>
-      <div class="calc-item">
-        <span class="calc-name"> 궁 3렙 : </span><span class="calc=value">{{ computeUltimateLevel3 }}</span>
-      </div>
+    <SummonerSpell/>
     </div>
   </div>
-  <SummonerSpell/>
-  <ChampionExp/>
 </template>
 
 <script>
 import champions from "../data/champions.json"
-import ChampionExp from "@/components/ChampionExp";
+import moment from "moment"
 import SummonerSpell from "@/components/SummonerSpell";
 export default {
   name: "AbilityHasteCalculator",
-  components: {SummonerSpell, ChampionExp},
+  components: {SummonerSpell},
   computed: {
     foundChampionInfo() {
       return champions.find(info => (
@@ -61,7 +79,7 @@ export default {
     computeUltimateLevel1() {
       if(this.foundChampionInfo) {
         let result = this.foundChampionInfo.ultimateLevel1 * 100 / ( 100 + this.inputHasteText + this.computeUltimateHunter)
-        return result.toFixed(2)
+        return result.toFixed(1)
       } else {
         return null
       }
@@ -69,7 +87,7 @@ export default {
     computeUltimateLevel2() {
       if(this.foundChampionInfo) {
         let result = this.foundChampionInfo.ultimateLevel2 * 100 / ( 100 + this.inputHasteText + this.computeUltimateHunter)
-        return result.toFixed(2)
+        return result.toFixed(1)
       } else {
         return null
       }
@@ -77,7 +95,7 @@ export default {
     computeUltimateLevel3() {
       if(this.foundChampionInfo) {
         let result = this.foundChampionInfo.ultimateLevel3 * 100 / ( 100 + this.inputHasteText + this.computeUltimateHunter)
-        return result.toFixed(2)
+        return result.toFixed(1)
       } else {
         return null
       }
@@ -92,8 +110,10 @@ export default {
   },
   data() {
     return {
+      moment,
       inputChampionName: "",
       inputHasteText: 0,
+      inputCurrentTime: "0000",
       ultimateCooldown: 6,
       useUltimateHunter: false,
       inputUltimateHunter: 0
@@ -113,21 +133,28 @@ export default {
   justify-content: center;
 
   .border-box{
+    padding: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    border: 1px solid rgba(0, 0, 0, 0.1);
+    border: 1px solid #eee;
     border-radius: 5px;
     background-color: white;
     box-shadow: 0 4px 16px rgba(29, 101, 160, 0.3);
   }
 
+  .item-container{
+    display: flex;
+    flex-direction: column;
+  }
+
   .input-area-container {
-    width: 320px;
-    height: 180px;
+    width: 200px;
+    height: 160px;
+    //justify-content: center;
 
     .input-item-container {
-      margin: 10px;
+      margin: 5px;
 
       .champion-names{
         width: 100px;
@@ -158,7 +185,7 @@ export default {
         }
       }
 
-      #ability-haste{
+      #ability-haste, #current-time{
         width: 70px;
         padding: 5px 10px;
         margin-left: 10px;
@@ -176,15 +203,25 @@ export default {
   }
 
   .calculator{
-    width: 170px;
-    height: 180px;
-    margin-left: 30px;
+    width: 200px;
+    height: 100px;
+    margin: auto;
+    border-bottom: 1px solid #87b1f3;
 
     .calc-item{
       display: flex;
-      margin: 5px 30px;
       align-items: center;
       justify-content: space-between;
+
+      &.small-text{
+        font-size: 14px;
+        color: #777;
+      }
+      span{
+        flex-basis: 0;
+        flex-grow: 1;
+        flex-shrink: 1;
+      }
     }
   }
 }
