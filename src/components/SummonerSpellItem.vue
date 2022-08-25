1cb6769a9836afe35112744def5c344404941199
rkdmf7220 <rkdmf7220@gmail.com>
2022-08-25 21:58:17 +0900

    소환사 주문 아이콘 및 선택 컴포넌트화

---
+++ src/components/SummonerSpellItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="spell-item-container">
+    <div class="spell-img"></div>
+    <ul class="select-spell-list">
+      <li class="select-spell-item">
+        <div class="spell-img"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SummonerSpellItem"
+}
+</script>
+
+<style scoped lang="scss">
+.spell-item-container{
+  width: 60px;
+  height: 60px;
+  position: relative;
+  cursor: pointer;
+  background-color: black;
+
+  &:first-child{
+    margin-right: 10px;
+  }
+}
+</style>
