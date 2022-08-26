25539560148fe6e81c26b1431e6f3372272d3de1
rkdmf7220 <rkdmf7220@gmail.com>
2022-08-26 22:35:34 +0900

    소환사 주문 변경 목록 구현

--- src/components/SummonerSpellItem.vue
+++ src/components/SummonerSpellItem.vue
@@ -2,9 +2,12 @@
   <div class="spell-item-container">
 <!--    <div class="spell-img"></div>-->
     <CooldownImageContainer :cooldown-data="cooldownData"/>
-    <ul class="select-spell-list">
-      <li class="select-spell-item">
-        <div class="spell-img"></div>
+    <button @click="isOpen = !isOpen" :class="{open: isOpen}" class="btn-spell-list">
+      <span class="btn-spell-icon"></span>
+    </button>
+    <ul :class="{open: isOpen}" class="select-spell-list">
+      <li v-for="item in SummonerSpells" :key="item.id" class="select-spell-item">
+        <div class="spell-img" :style="{ backgroundImage: 'url(/' + item.image + ')'}"></div>
       </li>
     </ul>
   </div>
@@ -12,14 +15,17 @@
 
 <script>
 import CooldownImageContainer from "@/components/CooldownImageContainer";
+import SummonerSpells from "../data/summonerSpells.json";
 export default {
   name: "SummonerSpellItem",
   components: {CooldownImageContainer},
   data() {
     return {
+      SummonerSpells,
+      isOpen: false,
       cooldownData: {
         thumb: "img/summonerSpell/mark.png",
-        cooldown: 10
+        cooldown: 48
       }
     }
   }
@@ -33,6 +39,61 @@ export default {
   height: 90px;
   position: relative;
 
+  .btn-spell-list{
+    width: 30px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    margin: 5px auto 0 auto;
+    box-sizing: border-box;
+    border: 2px solid #BAA371;
+    background-color: #196564;
+    cursor: pointer;
+    .btn-spell-icon{
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 12L15 5.33333L13.6 4L8 9.33333L2.4 4L1 5.33333L8 12Z' fill='%23BFAF4A'/%3E%3C/svg%3E%0A");
+    }
+    &.open{
+      .btn-spell-icon{
+        background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 4L1 10.6667L2.4 12L8 6.66667L13.6 12L15 10.6667L8 4Z' fill='%23BFAF4A'/%3E%3C/svg%3E%0A");
+      }
+    }
+  }
+  .select-spell-list{
+    width: 244px;
+    height: 314px;
+    display: none;
+    flex-wrap: wrap;
+    padding: 20px;
+    border: 2px solid #A28851;
+    box-sizing: border-box;
+    background-color: #031115;
+    position: absolute;
+    top: 90px;
+    left: 50%;
+    transform: translateX(-50%);
+    .select-spell-item{
+      width: 60px;
+      height: 60px;
+      margin: 0 10px 10px 0;
+      .spell-img{
+        width: 100%;
+        height: 100%;
+        border: 1px solid #A28851;
+        box-sizing: border-box;
+        background-size: cover;
+        cursor: pointer;
+      }
+      &:nth-child(3n){
+        margin-right: 0;
+      }
+    }
+    &.open{
+      display: flex;
+    }
+  }
   &:first-child{
     margin-right: 10px;
   }
