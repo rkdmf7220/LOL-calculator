51d2b4e644f7a632f209645f6aab33955440a5ac
rkdmf7220 <rkdmf7220@gmail.com>
2022-08-31 20:45:41 +0900

    vue router, vuex, axios 설치

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,13 @@
 <template>
-  <div class="calculator-list">
-    <div class="calculator-item">
-      <AbilityHasteCalculator/>
-    </div>
-    <div class="calculator-item">
-      <AbilityHasteCalculator/>
-    </div>
-    <div class="calculator-item">
-      <AbilityHasteCalculator/>
-    </div>
-    <div class="calculator-item">
-      <AbilityHasteCalculator/>
-    </div>
-    <div class="calculator-item">
-      <AbilityHasteCalculator/>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/simulator">Simulator</router-link>
   </div>
-  <ChampionExp/>
+  <router-view/>
 </template>
 
-<script>
-import AbilityHasteCalculator from "@/components/AbilityHasteCalculator";
-import ChampionExp from "@/components/ChampionExp";
-
-export default {
-  name: 'App',
-  components: {
-    ChampionExp,
-    AbilityHasteCalculator
-  }
-}
-</script>
-
 <style lang="scss">
-ul{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-button{
-  margin: 0;
-  padding: 0;
-}
 html{
   background: linear-gradient(135deg, #d3e4ff, #8fb1ff) no-repeat;
   min-height: 100vh;
@@ -55,13 +21,16 @@ html{
   margin-top: 60px;
   padding-bottom: 50px;
 }
-.calculator-list{
-  display: flex;
-  justify-content: center;
-  .calculator-item{
-    margin-right: 20px;
-    &:last-child{
-      margin-right: 0;
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
