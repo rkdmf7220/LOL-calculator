a1a48db680ea13ca4cc4c2552753c2caa103a0f7
rkdmf7220 <rkdmf7220@gmail.com>
2022-09-01 21:11:25 +0900

    home.vue 추가 및 간단 dispatch 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
     path: '/simulator',
     name: 'Simulator',
     component: () => import('../views/Simulator.vue')
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <input v-model="inputSummonerName" type="text" class="search-summoner" placeholder="소환사 명">
+      <button @click="this.dispatchInfo" class="submit"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      inputSummonerName: null
+    }
+  },
+  methods: {
+    dispatchInfo() {
+      this.$store.dispatch('getSummonerInfo', {summonerName: this.inputSummonerName})
+      // this.$store.dispatch('getSummonerInfo', (this.inputSummonerName))
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .search-bar{
+    width: 200px;
+    height: 40px;
+    position: relative;
+    .search-summoner{
+      width: 100%;
+      height: 100%;
+      padding: 5px 10px;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+    .submit{
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+
+    }
+  }
+</style>
