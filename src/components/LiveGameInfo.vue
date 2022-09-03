3aaafde28a0b3ae0c0272904213da746f96c19d6
rkdmf7220 <rkdmf7220@gmail.com>
2022-09-03 23:55:28 +0900

    인게임 정보 컴포넌트 생성

---
+++ src/components/LiveGameInfo.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <div class="live-game-container">
+    <div class="team-header blue-team">
+      <ul class="participant-list">
+        <li class="participant-item">
+          <ParticipantItem/>
+        </li>
+      </ul>
+    </div>
+    <div class="team-header red-team">
+      <ul class="participant-list">
+        <li class="participant-item">
+          <ParticipantItem/>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import ParticipantItem from "@/components/ParticipantItem";
+export default {
+  name: "LiveGameInfo",
+  components: {ParticipantItem},
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParticipantItem.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <div class="champion-img"></div>
+  <div class="summoner-spell"></div>
+  <div class="runes"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ParticipantItem"
+}
+</script>
+
+<style scoped>
+
+</style>
