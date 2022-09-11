8094f2289819915a46670b217a4f9f44ed8ceb80
LSM <lsm4551@on.mjc.ac.kr>
2022-09-11 18:38:38 +0900

    axios 호출 방식 변경

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex'
 import axios from "axios";
-const API_KEY = 'RGAPI-dc084813-509d-4b80-ad6b-a2599d1f869b'
+const API_KEY = 'RGAPI-d2f23ca9-dc26-4012-9c10-589d3b09861c'
 const URL = {
   summonerInfo : 'https://my-weather-server.herokuapp.com/https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/',
   activeGameInfo: 'https://my-weather-server.herokuapp.com/https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/',
@@ -34,7 +34,30 @@ export default createStore({
   },
   actions: {
     // api key 고정일 때
-    getSummonerInfo({commit, state}, {summonerName}) {
+    getSummonerInfo3({commit, state}, {summonerName}) {
+      const runTest = async () => {
+        let result = await axios.get(`${URL.summonerInfo}${summonerName}?api_key=${API_KEY}`);
+        console.log(result)
+        // something do it
+        return result
+      }
+      let res = runTest()
+      console.log('res ===> ',res)
+      console.log(commit, state)
+    },
+    async getSummonerInfo({commit, state}, {summonerName}) {
+      let result =  await axios.get(`${URL.summonerInfo}${summonerName}?api_key=${API_KEY}`);
+      if (result.statusText === "OK") {
+        console.log('result = ', result)
+        let item = result.data
+        let res = await axios.get(`${URL.activeGameInfo}${item.id}?api_key=${API_KEY}`)
+        console.log('res =', res)
+      }
+
+      console.log(commit, state, summonerName)
+    },
+
+    getSummonerInfo2({commit, state}, {summonerName}) {
       axios.get(`${URL.summonerInfo}${summonerName}?api_key=${API_KEY}`)
           .then(result => {
             if (result.statusText === "OK") {
@@ -50,6 +73,22 @@ export default createStore({
           })
       console.log(commit, state)
     },
+/*    getSummonerInfo({commit, state}, {summonerName}) {
+      axios.get(`${URL.summonerInfo}${summonerName}?api_key=${API_KEY}`)
+          .then(result => {
+            if (result.statusText === "OK") {
+              console.log(result)
+              let item = result.data
+              axios.get(`${URL.activeGameInfo}${item.id}?api_key=${API_KEY}`).then()
+              // let summonerId = item.accoundId
+              commit('summonerInfo', item)
+            } else {
+              // console.log('else로 작동')
+              // TODO: error 처리
+            }
+          })
+      console.log(commit, state)
+    },*/
     getSummonerId({commit, state}, {summonerId}) {
       axios.get(`${URL.summonerId}${summonerId}?api_key=${API_KEY}`)
           .then(result => {
